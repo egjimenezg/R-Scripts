7c7030a49a0ad03cb5681462692c5c3f42be984d
egjimenezg <egjimenezg@gmail.com>
2018-03-05 19:05:21 -0600

    Adding script that convers Excel files to .csv

---
+++ readFromExcel.R
@@ -0,0 +1,21 @@
+# install.packages(XLConnect)
+# sudo R CMD javareconf
+require(XLConnect)
+
+createCSVFiles <- function(path){
+  files <- list.files(path=".", pattern="*.xlsx")
+
+  for(file in files){
+    workbook <- loadWorkbook(file)
+    sheets = getSheets(workbook)
+    for(sheet in sheets){
+      data = readWorksheet(workbook,
+                           sheet=sheet)
+
+      write.csv(data, file=paste(tools::file_path_sans_ext(file),
+                                 "-",
+                                 sheet,".csv",sep=""))
+    }
+  }
+}
+
