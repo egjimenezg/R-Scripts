2da5b6890328cd25190768ede54ecc930636ca91
egjimenezg <egjimenezg@gmail.com>
2018-04-30 16:58:37 -0500

    Adding files path and setting more memory in the options

--- readFromExcel.R
+++ readFromExcel.R
@@ -1,21 +1,24 @@
 # install.packages(XLConnect)
 # sudo R CMD javareconf
+options(java.parameters = "-Xmx1024m")
+
 require(XLConnect)
 
-createCSVFiles <- function(path){
-  files <- list.files(path=".", pattern="*.xlsx")
+
+createCSVFiles <- function(filesPath){
+  files <- list.files(path=filesPath, pattern="*.xlsx")
 
   for(file in files){
-    workbook <- loadWorkbook(file)
+    workbook <- loadWorkbook(paste(c(filesPath,file),collapse="/"))
     sheets = getSheets(workbook)
     for(sheet in sheets){
       data = readWorksheet(workbook,
                            sheet=sheet)
 
       write.csv(data,
-                file=paste(tools::file_path_sans_ext(file),"-",sheet,".csv",sep=""),
+                file=paste(filesPath,"/",tools::file_path_sans_ext(file),"-",sheet,".csv",sep=""),
                 row.names=FALSE)
+      rm(data)
     }
   }
 }
-
